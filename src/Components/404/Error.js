362cccd09707dfe1b90f6e4c933905e932d07858
Lucas Moauro <lmoauro6@gmail.com>
2022-02-13 16:21:04 -0300

    Agregado de maquetacion y estilos al componente 404

--- src/Components/404/Error.js
+++ src/Components/404/Error.js
@@ -2,13 +2,20 @@ import { useNavigate } from "react-router-dom";
 
 export const Error = () => {
 	const navigate = useNavigate();
-	const handlePress = () => {
-		// console.log(e.key)
+	const handleClick = () => {
 		navigate("/");
 	};
 	return (
-		<div className="error" onClick={handlePress}>
-			<div></div>
+		<div className="error" onClick={handleClick}>
+			<div className="error__container">
+				<h2 className="error__title">Oh no! Page Not Found :(</h2>
+				<p className="error__paragraph">
+					The page you are looking for does not exist
+				</p>
+				<p className="error__paragraph">
+					Click anywhere to go back to the home page!
+				</p>
+			</div>
 		</div>
 	);
 };
